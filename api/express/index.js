7920488ea978d9ab723b592e207018d8fa290110
rios-matias <rios.matias998@gmail.com>
2024-04-12 23:52:00 -0300

    feat:: express, dom and cliente_servidor added

--- api/express/index.js
+++ api/express/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const books = require("../data/books");
 
+app.use(express.static('public'))
+app.get("/books", (req, res)=>{
+    res.status(200).send({
+        books
+    })
+})
+app.get("/dom",(req, res)=>{
+    res.redirect("/public/dom/index.hmtl")
+})
 app.get("/express", (req, res) => res.send("Express on Vercel!"));
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-app.use(express.static('public'))
+
+
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
